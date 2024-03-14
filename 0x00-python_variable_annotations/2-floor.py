5397bed219019692dda89be3aef5aa8c11215063
Ato Toffah <atotoffah@gmail.com>
2024-03-14 23:29:20 +0000

    Task 2:fix 1

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,9 +3,18 @@ import math
 
 """
 Defines an annotated floor function
+that returns the floor of a float
 """
 
 
 def floor(n: float) -> int:
-    """Takes a float n as argument and returns the floor of the float"""
+    """
+    Takes a float n as argument and returns the floor of the float
+
+    Args:
+        n (float): the number to be floored
+
+    Returns:
+        the floor of the float
+    """
     return math.floor(n)
