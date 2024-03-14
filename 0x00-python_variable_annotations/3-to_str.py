4d64fd95bebed8e2221c635f080bbee662e06490
Ato Toffah <atotoffah@gmail.com>
2024-03-14 22:48:40 +0000

    Task 3:fix 1

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,5 +6,6 @@ Defines an annotated concat function
 
 
 def to_str(n: float) -> str:
-    """Takes a float n as argument and returns the string representation of the float."""
+    """Takes a float n as argument and returns the
+    string representation of the float."""
     return str(n)
