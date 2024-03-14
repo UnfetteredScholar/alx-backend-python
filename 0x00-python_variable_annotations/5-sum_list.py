2e3f167f516014c80508081465e77d40999965bd
Ato Toffah <atotoffah@gmail.com>
2024-03-14 22:55:44 +0000

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+
+"""Defines the annotated function sum_list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list input_list of floats as
+    argument and returns their sum as a float"""
+    return sum(input_list)
