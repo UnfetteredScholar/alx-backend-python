8f11f3dbf360b7097f0d85a98da7f9ee1f3bdeaf
Ato Toffah <atotoffah@gmail.com>
2024-03-14 22:46:23 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+
+"""
+Defines an annotated concat function
+"""
+
+
+def floor(n: float) -> int:
+    """Takes a float n as argument and returns the floor of the float"""
+    return math.floor(n)
