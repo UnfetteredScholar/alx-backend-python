c3be17ea4130b70ace8608719e22031e8eb67725
Ato Toffah <atotoffah@gmail.com>
2024-03-18 19:35:50 +0000

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines a function task_wait_random"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+    """Creates an asyncio.Task for wait_random"""
+
+    times = await asyncio.gather(
+        *[task_wait_random(max_delay) for _ in range(0, n)]
+    )
+
+    return sorted(times)
